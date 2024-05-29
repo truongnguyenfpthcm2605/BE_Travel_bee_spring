600aa04643f85cb68740688e91d7b1d0cc3e4a6f
Nguyễn Văn Trường <truongnguenlqm@gmail.com>
2024-05-29 16:47:26 +0700

    fix

--- src/main/java/com/travelbee/app/repository/LocationRepository.java
+++ src/main/java/com/travelbee/app/repository/LocationRepository.java
@@ -2,11 +2,13 @@ package com.travelbee.app.repository;
 
 import com.travelbee.app.enities.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Locale;
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    @Query("select o from Location o where o.isactive = true order by o.createdate")
     List<Location> findAllByIsActiveTrue();
 }
